ac18ae02d3db4cd87e9386d4c2a7385f1ef333fa
Kai Mörker <kai.moerker@gmail.com>
2017-10-06 08:33:56 +0200

    implement Game class

--- Game.cs
+++ Game.cs
@@ -1,12 +1,42 @@
-﻿using System;
-
-public class Game
+﻿public class Game
 {
     private int id;
     private string name;
+    private Developer developer;
 
-    public Game()
+    public Game(int id, string name, Developer developer)
 	{
-            
+        this.id = id;
+        this.name = name;
+        this.developer = developer;
 	}
+
+    public Game(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Game(string name)
+    {
+        this.name = name;
+    }
+
+    public int ID
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+
+    public Developer Developer
+    {
+        get { return this.developer; }
+        set { this.developer = value; }
+    }
 }
