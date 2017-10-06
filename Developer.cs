636dfbd208627a0f489bb58e9321d87cb496bde2
Kai Mörker <kai.moerker@gmail.com>
2017-10-06 08:33:19 +0200

    implement Developer class

--- Developer.cs
+++ Developer.cs
@@ -1,8 +1,28 @@
-﻿using System;
-
-public class Class1
+﻿public class Developer
 {
-	public Class1()
+    private int id;
+    private string name;
+
+	public Developer(int id, string name)
 	{
+        this.id = id;
+        this.name = name;
 	}
+
+    public Developer(string name)
+    {
+        this.name = name;
+    }
+
+    public int ID
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
 }
