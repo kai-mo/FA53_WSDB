7a48215b3a0fe00bce898244eae67ba806bbaa3a
Tim Karliczek <tkarliczek@3pc.de>
2017-10-06 08:51:16 +0200

    add IDataAccess and IBusinessLayer interfaces

---
+++ IBusinessLayer.cs
@@ -0,0 +1,18 @@
+﻿interface IBusinessLayer
+{
+    void addGame(string name);
+
+    void addDeveloper(string name);
+
+    void editGame(string name);
+
+    void editDeveloper(string name);
+
+    void deleteGame(string name);
+
+    void deleteDeveloper(string name);
+
+    Game[] getGames();
+
+    Developer[] getDevelopers();
+}
---
+++ IDataAccess.cs
@@ -0,0 +1,18 @@
+﻿interface IDataAccess
+{
+    void add(Game game);
+
+    void add(Developer developer);
+
+    void edit(Game game);
+
+    void edit(Developer developer);
+
+    void delete(Game game);
+
+    void delete(Developer developer);
+
+    Game[] getGames();
+
+    Developer[] getDevelopers();
+}
