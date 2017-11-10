b82715bfbafb93f985250ee80364bb1bd95e7dfd
Kai Mörker <kai.moerker@gmail.com>
2017-11-10 12:21:24 +0100

    add realistic mock data and relations between them

--- DataAccess3.cs
+++ DataAccess3.cs
@@ -8,19 +8,22 @@ public class DataAccess3 : IDataAccess
 	
 	public DataAccess3()
 	{
-		games = new List<Game>();
-        games.Add(new Game(1, "EGameFive"));
-        games.Add(new Game(2, "AGameOne"));
-        games.Add(new Game(3, "BGameTwo"));
-        games.Add(new Game(4, "DGameFour"));
-        games.Add(new Game(5, "CGameThree"));
-		
 		developers = new List<Developer>();
-        developers.Add(new Developer(1, "EDeveloperOne"));
-        developers.Add(new Developer(2, "ADeveloperTwo"));
-        developers.Add(new Developer(3, "BDeveloperThree"));
-        developers.Add(new Developer(4, "DDeveloperFour"));
-        developers.Add(new Developer(5, "CDeveloperFive"));
+        developers.Add(new Developer(1, "Rockstar Games"));
+        developers.Add(new Developer(2, "Ubisoft"));
+        developers.Add(new Developer(3, "Valve"));
+        developers.Add(new Developer(4, "EA"));
+
+		games = new List<Game>();
+        games.Add(new Game(1, "Grand Theft Auto IV", developers[0]));
+        games.Add(new Game(2, "Grand Theft Auto V", developers[0]));
+        games.Add(new Game(3, "Assassin's Creed: Origins", developers[1]));
+        games.Add(new Game(4, "Anno 1404", developers[1]));
+        games.Add(new Game(5, "Counter-Strike: Global Offensive", developers[2]));	
+		games.Add(new Game(6, "FIFA 15", developers[3]));	
+		games.Add(new Game(7, "FIFA 16", developers[3]));	
+		games.Add(new Game(8, "FIFA 17", developers[3]));	
+		games.Add(new Game(9, "FIFA 18", developers[3]));	
 	}
 
     public void addGame(string name)
