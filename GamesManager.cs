89a3c038b8c5a260d66b6a5c66b4c29d72da0f1e
Kai Mörker <kai.moerker@gmail.com>
2017-11-30 12:40:45 +0100

    GamesManager.cs: add a general argument error message

--- GamesManager.cs
+++ GamesManager.cs
@@ -18,7 +18,7 @@ public class GamesManager
 			// TODO: implement DataAccess1 and DataAccess2 classes
             case "sqlite": dataAccess = new DataAccess1(); break;
             case "json": dataAccess = new DataAccess2(); break;
-			default: ShowDataAccessError(); return;
+			default: ShowArgumentError(); return;
 		}
 
 		IBusinessLayer businessLayer;
@@ -27,36 +27,21 @@ public class GamesManager
 		{
 			case "asc": businessLayer = new BusinessLayer1(dataAccess); break;
 			case "desc": businessLayer = new BusinessLayer2(dataAccess); break;
-			default: ShowBusinessLayerError(); return;
+			default: ShowArgumentError(); return;
 		}
 		
         switch (args[0].ToLower())
         {
             case "tui": new TUI(businessLayer); break;
             case "gui": Application.Run(new GUI()); break;
-            default: ShowViewError(); return;
+            default: ShowArgumentError(); return;
         }
     }
 
 	private static void ShowArgumentError()
 	{
-		Console.WriteLine("Please enter three arguments!");
-	}
-	
-	private static void ShowDataAccessError()
-	{
-		Console.WriteLine("Please enter a valid data access option [sqlite|json]!");
-	}
-	
-	private static void ShowBusinessLayerError()
-	{
-		Console.WriteLine("Please enter a valid businesslayer option [asc|desc]!");
-	}
-	
-    private static void ShowViewError()
-    {
-        Console.WriteLine("Please enter a valid view option [tui|gui]!");
+        Console.WriteLine("Invalid arguments!\n");
+        Console.WriteLine("Usage:\n");
+        Console.WriteLine("GamesManager.exe (tui|gui) (asc|desc) (sqlite|json)");
     }
-	
-
 }
