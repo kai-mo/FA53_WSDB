ee27a26b08ad72091c0e1fe005bc9cbf2e461a65
Kai Mörker <kai.moerker@gmail.com>
2017-12-01 09:07:26 +0100

    GUI: add listbox to display all games

--- GUI.cs
+++ GUI.cs
@@ -5,9 +5,33 @@ using System.Windows.Forms;
 public class GUI : Form
 {
     private IBusinessLayer businesslayer;
+    private ListBox lbxGames;
+    private GroupBox gbxGames;
 
     public GUI(IBusinessLayer businesslayer)
-	{
+    {
         this.businesslayer = businesslayer;
+
+        gbxGames = new GroupBox();
+        gbxGames.Text = "Games";
+        gbxGames.Location = new Point(10, 10);
+        gbxGames.Size = new Size(250, 350);
+        Controls.Add(gbxGames);
+
+        lbxGames = new ListBox();
+        lbxGames.Dock = DockStyle.Fill;
+        gbxGames.Controls.Add(lbxGames);
+
+        LoadGames();
+    }
+
+    private void LoadGames()
+    {
+        lbxGames.Items.Clear();
+
+        foreach (Game game in businesslayer.GetGames())
+        {
+            lbxGames.Items.Add(game.Name);
+        }
     }
 }
