b35a9258f8389676d278a92541d91447cb529245
Kai Mörker <kai.moerker@gmail.com>
2017-12-01 10:44:59 +0100

    GUI: add listbox to display all developers

--- GUI.cs
+++ GUI.cs
@@ -5,14 +5,18 @@ using System.Windows.Forms;
 public class GUI : Form
 {
     private IBusinessLayer businesslayer;
-    private ListBox lbxGames;
+
     private GroupBox gbxGames;
+    private GroupBox gbxDevelopers;
+
+    private ListBox lbxGames;
+    private ListBox lbxDevelopers;
 
     public GUI(IBusinessLayer businesslayer)
     {
         this.businesslayer = businesslayer;
         this.Text = "Games Manager";
-        this.Size = new Size(500, 500);
+        this.Size = new Size(545, 500);
 
         gbxGames = new GroupBox();
         gbxGames.Text = "Games";
@@ -20,11 +24,22 @@ public class GUI : Form
         gbxGames.Size = new Size(250, 350);
         Controls.Add(gbxGames);
 
+        gbxDevelopers = new GroupBox();
+        gbxDevelopers.Text = "Developers";
+        gbxDevelopers.Location = new Point(270, 10);
+        gbxDevelopers.Size = new Size(250, 350);
+        Controls.Add(gbxDevelopers);
+
         lbxGames = new ListBox();
         lbxGames.Dock = DockStyle.Fill;
         gbxGames.Controls.Add(lbxGames);
 
+        lbxDevelopers = new ListBox();
+        lbxDevelopers.Dock = DockStyle.Fill;
+        gbxDevelopers.Controls.Add(lbxDevelopers);
+
         LoadGames();
+        LoadDevelopers();
     }
 
     private void LoadGames()
@@ -34,6 +49,16 @@ public class GUI : Form
         foreach (Game game in businesslayer.GetGames())
         {
             lbxGames.Items.Add(game.Name);
+        }
+    }
+
+    private void LoadDevelopers()
+    {
+        lbxDevelopers.Items.Clear();
+
+        foreach (Developer developer in businesslayer.GetDevelopers())
+        {
+            lbxDevelopers.Items.Add(developer.Name);
         }
     }
 }
