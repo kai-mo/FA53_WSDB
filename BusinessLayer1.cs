6a0378828543ed8238361b983629f69c86252c84
Kai Mörker <kai.moerker@gmail.com>
2017-12-19 19:54:36 +0100

    BusinessLayer1: apply code styles

--- BusinessLayer1.cs
+++ BusinessLayer1.cs
@@ -5,7 +5,8 @@ namespace GamesManager
     public class BusinessLayer1 : AbstractBusinessLayer
     {
         public BusinessLayer1(IDataAccess dataAccess) : base(dataAccess)
-        { }
+        {
+        }
 
         public override List<Game> GetGames()
         {
